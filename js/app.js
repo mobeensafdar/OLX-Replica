b67f17812f1f1085cab70d1c7981c678a07e08a4
mobeensafdar <mobeensafdar21@gmail.com>
2020-10-28 11:43:03 +0500

    key remove

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDdmzaGN7Eg60g_evT-JUbMnP0Hd4h7vl4",
-    authDomain: "olx-pk18.firebaseapp.com",
-    databaseURL: "https://olx-pk18.firebaseio.com",
-    projectId: "olx-pk18",
-    storageBucket: "olx-pk18.appspot.com",
-    messagingSenderId: "347156467683",
-    appId: "1:347156467683:web:c3e72035d96eeecf"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 
 
 };
